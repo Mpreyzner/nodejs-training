cb85305b06344e212c3f4712575220348b19c06f
Mpreyzner <maria.preyzner@gmail.com>
2017-03-22 11:13:00 +0100

    Add route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+    const status = 201;
+    res.status(status);
+    res.message('User created');
+
+    res.send({status, message: res.message})
 });
 
+router.post('/', function (req, res, next) {
+
+    const status = 201;
+    res.status(status);
+    res.message('User created');
+
+    res.send({status, message: res.message})
+    // status: status equals to status
+});
 module.exports = router;
